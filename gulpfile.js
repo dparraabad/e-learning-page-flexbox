c4cf00f7b3b6c2106027263dc187e32d42e69128
Diego Parra <dparraabad@gmail.com>
2020-12-07 19:30:32 -0500

    Delete comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,25 @@
-//Aquí irá la configuración de Gulp (Task Manager): funciona con tareas y cada tarea es una función que tendrá una instrucción que se ejecutará después de otras.
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-
-//Esta función hará que al archivo seleccionado "app.scss" se le aplicará un autoprefixer y 
 function css() {
   return gulp
-    .src('scss/main.scss') //Servirá para enlazar el archivo de app.scss con este archivo para agregar los pipes
+    .src('scss/main.scss') 
     .pipe(autoprefixer({
-        overrideBrowserslist : ['last 2 versions'], //Hacer autoprefixer a todo lo que requiera las últimas 2 versiones del navegador
+        overrideBrowserslist : ['last 2 versions'],
         cascade: false
     }))
     .pipe(sass({
-      outputStyle: 'expanded', //nested, compact or compressed (minificado)
+      outputStyle: 'expanded',
     }))
-    .pipe(gulp.dest('css')); //carpeta destino que se estará generando después de todas las operaciones realizadas.
+    .pipe(gulp.dest('css')); 
 }
 
 function watchFiles() {
-  gulp.watch('scss/*.scss', css); //Cada vez que ocurra algún cambio se estará ejecutando la función de "css" por tanto se compilará de nuevo.
+  gulp.watch('scss/*.scss', css);
   gulp.watch('index.html')
 }
 
 
-//Registrar funciones como tareas
-gulp.task('css', css); //Se llamará "css" y se registra en la función "css()"
-// gulp.task('watch', watchFiles);
-gulp.task('watch', gulp.parallel(watchFiles)); //Dentro de parallel estarán todas las tareas que se estarán corriendo en paralelo permitiendo mejorar el performance.
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
